e23aea8f2f616041fbdfcf69288c026a315b2dbc
相商 <oncwnuLNuclItdHXxQA8u7gv7Dt0@git.weixin.qq.com>
2020-06-05 12:10:30 +0800

    1.使用了各种lambda中可能用到各种表达式

---
+++ src/main/java/com/github/greekpanda/jdk8/lambda/MyFun.java
@@ -0,0 +1,11 @@
+package com.github.greekpanda.jdk8.lambda;
+
+/**
+ * @author GreekPanda
+ * @version 1.0
+ * @date 2020/6/5 0005 下午 12:04
+ */
+@FunctionalInterface
+public interface MyFun {
+    Integer getValue(Integer num);
+}
